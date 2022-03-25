354ba6f2d2aa3de59efaf09cc1b494d241222760
rabiulim <rabiulim2022@gmail.com>
2022-03-25 22:21:57 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>My assignment start</h1>
+      <Header></Header>
     </div>
   );
 }
