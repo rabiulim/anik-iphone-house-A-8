94d0a758b8e6b5c8143b582414535847884308cf
rabiulim <rabiulim2022@gmail.com>
2022-03-25 22:34:19 +0600

    header style added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h1>A1 Book Corner</h1>
         </div>
     );
